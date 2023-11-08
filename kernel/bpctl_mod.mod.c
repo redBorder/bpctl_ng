803abb67a365c7f7a8927225ca4ae40d423cdbb3
David Vanhoucke <dvanhoucke@redborder.com>
2023-11-08 12:03:49 +0000

    dkms updates

--- kernel/bpctl_mod.mod.c
+++ kernel/bpctl_mod.mod.c
@@ -1,11 +1,18 @@
 #include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/elfnote-lto.h>
 #include <linux/vermagic.h>
 #include <linux/compiler.h>
 
-MODULE_INFO(vermagic, VERMAGIC_STRING);
+BUILD_SALT;
+BUILD_LTO_INFO;
 
-struct module __this_module
-__attribute__((section(".gnu.linkonce.this_module"))) = {
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
 	.name = KBUILD_MODNAME,
 	.init = init_module,
 #ifdef CONFIG_MODULE_UNLOAD
@@ -14,73 +21,68 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 	.arch = MODULE_ARCH_INIT,
 };
 
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
 static const struct modversion_info ____versions[]
-__used
-__attribute__((section("__versions"))) = {
-	{ 0x28950ef1, __VMLINUX_SYMBOL_STR(module_layout) },
-	{ 0x6bc3fbc0, __VMLINUX_SYMBOL_STR(__unregister_chrdev) },
-	{ 0xd2b09ce5, __VMLINUX_SYMBOL_STR(__kmalloc) },
-	{ 0xf9a482f9, __VMLINUX_SYMBOL_STR(msleep) },
-	{ 0x4c4fef19, __VMLINUX_SYMBOL_STR(kernel_stack) },
-	{ 0x349cba85, __VMLINUX_SYMBOL_STR(strchr) },
-	{ 0x16a5a12f, __VMLINUX_SYMBOL_STR(single_open) },
-	{ 0x68f1a3f7, __VMLINUX_SYMBOL_STR(i2c_smbus_read_i2c_block_data) },
-	{ 0x2296f507, __VMLINUX_SYMBOL_STR(single_release) },
-	{ 0x9f6b4c62, __VMLINUX_SYMBOL_STR(down_interruptible) },
-	{ 0x74df1d4, __VMLINUX_SYMBOL_STR(seq_printf) },
-	{ 0xd2da1048, __VMLINUX_SYMBOL_STR(register_netdevice_notifier) },
-	{ 0xa16aae11, __VMLINUX_SYMBOL_STR(remove_proc_entry) },
-	{ 0xc816cb3, __VMLINUX_SYMBOL_STR(__register_chrdev) },
-	{ 0xeae3dfd6, __VMLINUX_SYMBOL_STR(__const_udelay) },
-	{ 0x593a99b, __VMLINUX_SYMBOL_STR(init_timer_key) },
-	{ 0x91715312, __VMLINUX_SYMBOL_STR(sprintf) },
-	{ 0x9c3df9b4, __VMLINUX_SYMBOL_STR(seq_read) },
-	{ 0x7d11c268, __VMLINUX_SYMBOL_STR(jiffies) },
-	{ 0x9d0d6206, __VMLINUX_SYMBOL_STR(unregister_netdevice_notifier) },
-	{ 0x71de9b3f, __VMLINUX_SYMBOL_STR(_copy_to_user) },
-	{ 0x44f0d59d, __VMLINUX_SYMBOL_STR(PDE_DATA) },
-	{ 0xd5f2172f, __VMLINUX_SYMBOL_STR(del_timer_sync) },
-	{ 0x10f1064d, __VMLINUX_SYMBOL_STR(kstrtoint_from_user) },
-	{ 0xbe872f4b, __VMLINUX_SYMBOL_STR(pci_get_subsys) },
-	{ 0x73839c7a, __VMLINUX_SYMBOL_STR(proc_mkdir) },
-	{ 0x8f64aa4, __VMLINUX_SYMBOL_STR(_raw_spin_unlock_irqrestore) },
-	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
-	{ 0x20c55ae0, __VMLINUX_SYMBOL_STR(sscanf) },
-	{ 0x8834396c, __VMLINUX_SYMBOL_STR(mod_timer) },
-	{ 0x8070df92, __VMLINUX_SYMBOL_STR(init_net) },
-	{ 0xac1a55be, __VMLINUX_SYMBOL_STR(unregister_reboot_notifier) },
-	{ 0x42c8de35, __VMLINUX_SYMBOL_STR(ioremap_nocache) },
-	{ 0x3517383e, __VMLINUX_SYMBOL_STR(register_reboot_notifier) },
-	{ 0xf0fdf6cb, __VMLINUX_SYMBOL_STR(__stack_chk_fail) },
-	{ 0x2ea2c95c, __VMLINUX_SYMBOL_STR(__x86_indirect_thunk_rax) },
-	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
-	{ 0x9327f5ce, __VMLINUX_SYMBOL_STR(_raw_spin_lock_irqsave) },
-	{ 0x5ae98fc6, __VMLINUX_SYMBOL_STR(remove_proc_subtree) },
-	{ 0x8c34c149, __VMLINUX_SYMBOL_STR(proc_create_data) },
-	{ 0x4f68e5c9, __VMLINUX_SYMBOL_STR(do_gettimeofday) },
-	{ 0x1685c91c, __VMLINUX_SYMBOL_STR(seq_lseek) },
-	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
-	{ 0xedc03953, __VMLINUX_SYMBOL_STR(iounmap) },
-	{ 0x71e3cecb, __VMLINUX_SYMBOL_STR(up) },
-	{ 0x7bfc830b, __VMLINUX_SYMBOL_STR(i2c_smbus_write_i2c_block_data) },
-	{ 0x77e2f33, __VMLINUX_SYMBOL_STR(_copy_from_user) },
-	{ 0x9e7d6bd0, __VMLINUX_SYMBOL_STR(__udelay) },
-	{ 0x88db9f48, __VMLINUX_SYMBOL_STR(__check_object_size) },
-	{ 0x760a0f4f, __VMLINUX_SYMBOL_STR(yield) },
-	{ 0xe914e41e, __VMLINUX_SYMBOL_STR(strcpy) },
+__used __section("__versions") = {
+	{ 0x3af7223e, "module_layout" },
+	{ 0x6bc3fbc0, "__unregister_chrdev" },
+	{ 0xeb233a45, "__kmalloc" },
+	{ 0xf9a482f9, "msleep" },
+	{ 0x349cba85, "strchr" },
+	{ 0x33267c2e, "single_open" },
+	{ 0x1ed8b3cb, "i2c_smbus_read_i2c_block_data" },
+	{ 0xdf0256ec, "single_release" },
+	{ 0x6bd0e573, "down_interruptible" },
+	{ 0xf5f7945b, "seq_printf" },
+	{ 0xd2da1048, "register_netdevice_notifier" },
+	{ 0x83ba136e, "remove_proc_entry" },
+	{ 0x2a4094da, "__register_chrdev" },
+	{ 0xeae3dfd6, "__const_udelay" },
+	{ 0x78534f62, "init_timer_key" },
+	{ 0x4629334c, "__preempt_count" },
+	{ 0x3c3ff9fd, "sprintf" },
+	{ 0xa35a9cc0, "seq_read" },
+	{ 0x15ba50a6, "jiffies" },
+	{ 0x9d0d6206, "unregister_netdevice_notifier" },
+	{ 0x6b10bee1, "_copy_to_user" },
+	{ 0x5b8239ca, "__x86_return_thunk" },
+	{ 0xcd3c7696, "del_timer_sync" },
+	{ 0xf9ca2eb4, "kstrtoint_from_user" },
+	{ 0x5de4a7c6, "pci_get_subsys" },
+	{ 0x591c81b7, "proc_mkdir" },
+	{ 0xd35cce70, "_raw_spin_unlock_irqrestore" },
+	{ 0xbcab6ee6, "sscanf" },
+	{ 0xde80cd09, "ioremap" },
+	{ 0x3cf85989, "mod_timer" },
+	{ 0x28bbb0e9, "init_net" },
+	{ 0xac1a55be, "unregister_reboot_notifier" },
+	{ 0xa916b694, "strnlen" },
+	{ 0x3517383e, "register_reboot_notifier" },
+	{ 0xd0da656b, "__stack_chk_fail" },
+	{ 0x92997ed8, "_printk" },
+	{ 0x65487097, "__x86_indirect_thunk_rax" },
+	{ 0xbdfb6dbb, "__fentry__" },
+	{ 0xcbd4898c, "fortify_panic" },
+	{ 0x34db050b, "_raw_spin_lock_irqsave" },
+	{ 0x9e4e29e2, "remove_proc_subtree" },
+	{ 0xaa101e4c, "proc_create_data" },
+	{ 0x42cd7ed2, "seq_lseek" },
+	{ 0x37a0cba, "kfree" },
+	{ 0xedc03953, "iounmap" },
+	{ 0xcf2a6966, "up" },
+	{ 0x927152bb, "i2c_smbus_write_i2c_block_data" },
+	{ 0x13c49cc2, "_copy_from_user" },
+	{ 0x9e7d6bd0, "__udelay" },
+	{ 0x88db9f48, "__check_object_size" },
+	{ 0x760a0f4f, "yield" },
+	{ 0xe914e41e, "strcpy" },
 };
 
-static const char __module_depends[]
-__used
-__attribute__((section(".modinfo"))) =
-"depends=";
+MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "444090078559BB479F74F66");
-MODULE_INFO(rhelversion, "7.9");
-#ifdef RETPOLINE
-	MODULE_INFO(retpoline, "Y");
-#endif
-#ifdef CONFIG_MPROFILE_KERNEL
-	MODULE_INFO(mprofile, "Y");
-#endif
+MODULE_INFO(srcversion, "AFE78E3D9FA2EB6478F9112");
+MODULE_INFO(rhelversion, "9.2");
